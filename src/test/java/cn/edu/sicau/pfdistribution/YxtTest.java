8005973442473e98aa5ad946be7453ee53a63dd0
Merlin <yellowicq@qq.com>
2019-04-24 20:48:04 +0800

    add yxt data

--- src/test/java/cn/edu/sicau/pfdistribution/YxtTest.java
+++ src/test/java/cn/edu/sicau/pfdistribution/YxtTest.java
@@ -1,8 +1,11 @@
-package cn.edu.sicau.pfdistribution.dao.yxt;
+package cn.edu.sicau.pfdistribution;
 
+import cn.edu.sicau.pfdistribution.dao.yxt.YxtMain;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
