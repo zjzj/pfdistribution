2d0f0a2681a7eff279b2a84faa62fb4851314734
Merlin <yellowicq@qq.com>
2019-04-24 20:42:09 +0800

    add yxt data

--- src/main/java/cn/edu/sicau/pfdistribution/dao/yxt/YxtTest.java
+++ src/main/java/cn/edu/sicau/pfdistribution/dao/yxt/YxtTest.java
@@ -1,21 +1,36 @@
 package cn.edu.sicau.pfdistribution.dao.yxt;
 
-import java.sql.*;
-import java.util.Arrays;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-public class YxtTest
-{
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YxtTest {
 
-    public static void main(String[] args)
-    {
+    @Autowired
+    private YxtMain yxtMain;
 
+    @Before
+    public void init() {
+        System.out.println("开始测试-----------------");
+    }
+
+    @After
+    public void after() {
+        System.out.println("测试结束-----------------");
+    }
+
+    @Test
+    public void testYxtMain() {
         //显示已读数据
         int i = 0;
-        for(i=1;i<czzs;i++)
-        {
-            if(cz[i] == null)
+        for (i = 1; i < yxtMain.czzs; i++) {
+            if (yxtMain.cz[i] == null)
                 continue;
-            System.out.println(cz[i].czm);
+            System.out.println(yxtMain.cz[i].czm);
         }
     }
 
