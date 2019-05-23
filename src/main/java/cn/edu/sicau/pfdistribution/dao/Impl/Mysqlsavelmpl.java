1ff168ab2f865bf4a5ff2a31b942c2a3b36868e8
xieyuan101 <1184536952@qq.com>
2019-05-23 19:18:14 +0800

    Oracle数据库2

--- src/main/java/cn/edu/sicau/pfdistribution/dao/Impl/Mysqlsavelmpl.java
+++ src/main/java/cn/edu/sicau/pfdistribution/dao/Impl/Mysqlsavelmpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public class  Mysqlsavelmpl implements RegionSaveInterface {
--- src/main/java/cn/edu/sicau/pfdistribution/dao/Impl/OracleImpl.java
+++ src/main/java/cn/edu/sicau/pfdistribution/dao/Impl/OracleImpl.java
@@ -16,17 +16,17 @@ import java.util.Map;
 public class OracleImpl implements OracleGetod {
     @Autowired
     @Qualifier("oracleJdbcTemplate")
-    private JdbcTemplate jdbcTemplate2;
-    //private JdbcTemplate jdbcTemplate;  //这个是系统自带的
+    private JdbcTemplate jdbcTemplate;
+
     @Override
     public void deleteCare(){
-        jdbcTemplate2.update("delete   \n" +
+        jdbcTemplate.update("delete   \n" +
                 "from \"SCOTT\".\"ceshi_copy1\"\n" +
                 "WHERE \"票卡种类\" not like '%票%' and \"票卡种类\" not like '%卡%'\n");
     }
     @Override
     public void creatGet(){
-        jdbcTemplate2.update("CREATE TABLE \"SCOTT\".\"test_od\" (   \n" +
+        jdbcTemplate.update("CREATE TABLE \"SCOTT\".\"test_od\" (   \n" +
                 "  \"票卡号\" CHAR(20) ,\n" +
                 "  \"进站点\" CHAR(100) ,\n" +
                 "  \"进站时间\" TIMESTAMP ,\n" +
@@ -36,7 +36,7 @@ public class OracleImpl implements OracleGetod {
     }
     @Override
     public void creatIn(){
-        jdbcTemplate2.update("CREATE TABLE \"SCOTT\".\"test_in\" (   --创建储存进站数据表\n" +
+        jdbcTemplate.update("CREATE TABLE \"SCOTT\".\"test_in\" (   --创建储存进站数据表\n" +
                 "  \"票卡号\" CHAR(20) ,\n" +
                 "  \"进站点\" CHAR(100) ,\n" +
                 "  \"进站时间\" TIMESTAMP ,\n" +
@@ -45,7 +45,7 @@ public class OracleImpl implements OracleGetod {
     }
     @Override
     public void creatOut(){
-        jdbcTemplate2.update("CREATE TABLE \"SCOTT\".\"test_out\" (  \n" +
+        jdbcTemplate.update("CREATE TABLE \"SCOTT\".\"test_out\" (  \n" +
                 "  \"票卡号\" CHAR(20) ,\n" +
                 "  \"出站点\" CHAR(100) ,\n" +
                 "  \"出站时间\" TIMESTAMP ,\n" +
@@ -54,21 +54,21 @@ public class OracleImpl implements OracleGetod {
     }
     @Override
     public void insertIn(){
-        jdbcTemplate2.update("insert into \"SCOTT\".\"test_in\"(\"票卡号\",\"进站点\",\"进站时间\",\"交易前金额(元)\")  --插入进站数据到进站数据表\n" +
+        jdbcTemplate.update("insert into \"SCOTT\".\"test_in\"(\"票卡号\",\"进站点\",\"进站时间\",\"交易前金额(元)\")  --插入进站数据到进站数据表\n" +
                 "select \"票卡号\",\"交易车站\",\"数据接收时间\",\"交易前余额(元)\"\n" +
                 "from \"SCOTT\".\"ceshi_copy1\"\n" +
                 "where \"交易事件\"='进站'");
     }
     @Override
     public void insertOut(){
-        jdbcTemplate2.update("insert into \"SCOTT\".\"test_out\"(\"票卡号\",\"出站点\",\"出站时间\",\"交易前金额(元)\")  --插入出站数据到出站数据表\n" +
+        jdbcTemplate.update("insert into \"SCOTT\".\"test_out\"(\"票卡号\",\"出站点\",\"出站时间\",\"交易前金额(元)\")  --插入出站数据到出站数据表\n" +
                 "select \"票卡号\",\"交易车站\",\"数据接收时间\",\"交易前余额(元)\"\n" +
                 "from \"SCOTT\".\"ceshi_copy1\"\n" +
                 "where \"交易事件\"='出站'");
     }
     @Override
     public void insertOd(){
-        jdbcTemplate2.update("insert into \"SCOTT\".\"test_od\"        --查找OD数据并插入到OD结果数据表\n" +
+        jdbcTemplate.update("insert into \"SCOTT\".\"test_od\"        --查找OD数据并插入到OD结果数据表\n" +
                 "SELECT \"SCOTT\".\"test_in\".\"票卡号\",\"SCOTT\".\"test_in\".\"进站点\",\"SCOTT\".\"test_in\".\"进站时间\",\"SCOTT\".\"test_out\".\"出站点\",\"SCOTT\".\"test_out\".\"出站时间\"\n" +
                 "FROM \"SCOTT\".\"test_in\",\"SCOTT\".\"test_out\"\n" +
                 "WHERE \"SCOTT\".\"test_in\".\"票卡号\"=\"SCOTT\".\"test_out\".\"票卡号\" AND \"SCOTT\".\"test_in\".\"交易前金额(元)\"=\"SCOTT\".\"test_out\".\"交易前金额(元)\"\n" +
@@ -76,26 +76,26 @@ public class OracleImpl implements OracleGetod {
     }
     @Override
     public void deleteNot(){
-        jdbcTemplate2.update("delete \n" +
+        jdbcTemplate.update("delete \n" +
                 "from \"SCOTT\".\"test_od\"\n" +
                 "where TO_CHAR(\"出站时间\")<=TO_CHAR(\"进站时间\") ");
     }
     @Override
     public void deleteIn(){
-        jdbcTemplate2.update("DROP TABLE \"SCOTT\".\"test_in\"\n");
+        jdbcTemplate.update("DROP TABLE \"SCOTT\".\"test_in\"\n");
     }
     @Override
     public void deleteOut() {
-        jdbcTemplate2.update("DROP TABLE \"SCOTT\".\"test_out\"\n");
+        jdbcTemplate.update("DROP TABLE \"SCOTT\".\"test_out\"\n");
     }
     @Override
     public void deleteOd(){
-        jdbcTemplate2.update("DROP TABLE \"SCOTT\".\"test_od\"\n");
+        jdbcTemplate.update("DROP TABLE \"SCOTT\".\"test_od\"\n");
     }
     @Override
     public List<String> selectOd(String selectOD){
         List<String> strList = new ArrayList<>();
-        List rows= jdbcTemplate2.queryForList(selectOD);
+        List rows= jdbcTemplate.queryForList(selectOD);
         Iterator it = rows.iterator();
         while(it.hasNext()) {
             Map userMap = (Map) it.next();
