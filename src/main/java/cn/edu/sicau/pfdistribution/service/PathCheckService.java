eaf096baf854be5c37a4c9eaf81f33eb9fbc9709
hyc <Q2092686598@163.com>
2019-05-23 21:30:49 +0800

    k路径计算

--- src/main/java/cn/edu/sicau/pfdistribution/service/PathCheckService.java
+++ src/main/java/cn/edu/sicau/pfdistribution/service/PathCheckService.java
@@ -1,7 +1,9 @@
 package cn.edu.sicau.pfdistribution.service;
 
+import cn.edu.sicau.pfdistribution.service.kspcalculation.Edge;
 import cn.edu.sicau.pfdistribution.service.kspcalculation.util.Path;
 
-public interface SectionCheckService {
+public interface PathCheckService {
     boolean checkPath(Path path);
+    boolean checkSection(Edge section);
 }
--- src/main/java/cn/edu/sicau/pfdistribution/service/SectionCheckServiceImpl.java
+++ src/main/java/cn/edu/sicau/pfdistribution/service/SectionCheckServiceImpl.java
@@ -7,11 +7,33 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class SectionCheckServiceImpl implements SectionCheckService{
+public class SectionCheckServiceImpl implements PathCheckService{
 
+    /**
+     * 返回false表示路径不通
+     * @param path
+     * @return
+     */
     @Override
     public boolean checkPath(Path path) {
         List<Edge> edges = path.getEdges();
+        boolean flag = true;
+        for(int i = 0; i < edges.size(); i++){
+            if(!checkSection(edges.get(i))){
+                flag = false;
+                break;
+            }
+        }
+        return flag;
+    }
+
+    /**
+     * 返回false表示区间不通
+     * @param section
+     * @return
+     */
+    @Override
+    public boolean checkSection(Edge section) {
         return true;
     }
 }
