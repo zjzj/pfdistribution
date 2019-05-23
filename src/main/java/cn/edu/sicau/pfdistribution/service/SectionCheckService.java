4f4a4bfaa139203b42746a3fac269c620c36aed5
hyc <Q2092686598@163.com>
2019-05-23 21:18:08 +0800

    k路径计算

---
+++ src/main/java/cn/edu/sicau/pfdistribution/service/SectionCheckService.java
@@ -0,0 +1,7 @@
+package cn.edu.sicau.pfdistribution.service;
+
+import cn.edu.sicau.pfdistribution.service.kspcalculation.util.Path;
+
+public interface SectionCheckService {
+    boolean checkPath(Path path);
+}
