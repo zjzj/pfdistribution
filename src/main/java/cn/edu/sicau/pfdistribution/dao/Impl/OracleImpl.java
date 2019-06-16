63286c800818e925f2092a840931f81540ff123a
xieyuan101 <1184536952@qq.com>
2019-06-16 22:12:17 +0800

    动态路径计算更新

--- src/main/java/cn/edu/sicau/pfdistribution/dao/Impl/OracleImpl.java
+++ src/main/java/cn/edu/sicau/pfdistribution/dao/Impl/OracleImpl.java
@@ -144,4 +144,5 @@ public class OracleImpl implements OracleGetod {
         }
         return strings;
     }
+
 }
--- src/main/java/cn/edu/sicau/pfdistribution/dao/mysqlsave/SelectLineid.java
+++ src/main/java/cn/edu/sicau/pfdistribution/dao/mysqlsave/SelectLineid.java
@@ -1,13 +1,13 @@
 package cn.edu.sicau.pfdistribution.dao.mysqlsave;
 
 import lombok.Data;
-import java.math.BigDecimal;
+
 
 @Data
 public class SelectLineid {
-    public BigDecimal LINE_ID;
+    public int LINE_ID;
     public String LINE_NAME;
-    public BigDecimal CZ_SN;
-    public BigDecimal CZ_ID;
+    public int CZ_SN;
+    public int CZ_ID;
     public String CZ_NAME;
 }
